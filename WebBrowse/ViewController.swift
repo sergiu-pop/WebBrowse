6697593b90ef16a5b9c58035a3f64f621de164f3
Sergiu <sergiu.pop@saltandpepper.co>
2019-01-30 10:52:57 +0200

    Initial Commit

---
+++ WebBrowse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebBrowse
+//
+//  Created by Sergiu on 30/01/2019.
+//  Copyright © 2019 Sergiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
